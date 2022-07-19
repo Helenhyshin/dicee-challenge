28dd5077c1cd7b0faf3439211a6d19b1066c786b
Helen Shin <helenhyshin@gmail.com>
2022-07-19 14:40:49 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+let dice1 = document
+  .querySelector("img")
+  .setAttribute("src", `./images/dice${randomNumber1}.png`);
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let dice2 = document
+  .querySelector("img.img2")
+  .setAttribute("src", `./images/dice${randomNumber2}.png`);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw";
+}
